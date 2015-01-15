36984f625cc05ad0d710dafcaa4132eacdc9843c
hoog <cosinux@gmail.com>
2015-01-15 16:43:12 +0100

    retest checkin

--- trucks/admin.py
+++ trucks/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from trucks.models import Truck,Trailer,WheelTruck,WheelTrailer
 # Register your models here.
+
+class TruckAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['plate_number','comments','trailer']}),
+        ('Dates', {'fields': ['maintenance_date']}),
+    ]
+
+class TrailerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['plate_number','comments']}),
+        ('Dates', {'fields': ['maintenance_date']}),
+    ]
+
+admin.site.register(Truck,TruckAdmin)
+admin.site.register(Trailer,TrailerAdmin)
+admin.site.register(WheelTruck)
+admin.site.register(WheelTrailer)
+
--- trucks/models.py
+++ trucks/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 
-# Create your models here.
 class Truck(models.Model):
+    plate_number = models.CharField(primary_key=True,max_length=32,default="RW SH ..")
     comments = models.CharField(max_length=200)
-    maintenance_date = models.DateTimeField('TÜV Datum')
+    maintenance_date = models.DateTimeField('Maintenance Date')
+    trailer = models.ForeignKey(Trailer)
 
-class Wheels(models.Model):
+    def __str__(self):
+        return self.plate_number
+
+class Trailer(models.Model):
+    plate_number = models.CharField(primary_key=True,max_length=32,default="RW SH ..")
+    comments = models.CharField(max_length=200)
+    maintenance_date = models.DateTimeField('Maintenance Date')
+
+    def __str__(self):
+        return self.plate_number
+
+
+class WheelTruck(models.Model):
     truck = models.ForeignKey(Truck)
+    position = models.CharField(max_length=64)
+    distance = models.IntegerField(default=0)
+
+class WheelTrailer(models.Model):
+    trailer = models.ForeignKey(Trailer)
     position = models.CharField(max_length=64)
     distance = models.IntegerField(default=0)
